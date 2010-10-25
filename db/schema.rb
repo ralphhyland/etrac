2282f7ae05677d76a98a5554dbff00423c1f89f8
Ralph Hyland <ralph.hyland@gmail.com>
2010-10-25 21:04:12 +0100

    Users changes

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101025163405) do
+ActiveRecord::Schema.define(:version => 20101025191317) do
 
   create_table "students", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
